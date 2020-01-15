69963c791743ae9970194c80439d9bf9dc473500
QueenOfTheUnicorns-new <59733837+QueenOfTheUnicorns-new@users.noreply.github.com>
2020-01-15 16:51:08 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends FragmentActivity {
+private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn1);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                Fragment_b fragmentB = new Fragment_b();
+
+                ft.add(R.id.hold_space, fragmentB);
+                ft.commit();
+            }
+        });
+
+
+    }
+}
